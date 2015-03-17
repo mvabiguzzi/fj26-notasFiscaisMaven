b0142be86361e02b877831d660bc89755c3dc1ca
Marcos Vinícius Almeida Biguzzi <contato@mvabiguzzi.com.br>
2015-03-17 15:38:40 -0300

    criação da listagem de produtos

--- src/main/java/br/com/mvabiguzzi/mb/ProdutoBean.java
+++ src/main/java/br/com/mvabiguzzi/mb/ProdutoBean.java
@@ -1,5 +1,7 @@
 package br.com.mvabiguzzi.mb;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 
 import br.com.mvabiguzzi.notasfiscais.dao.ProdutoDao;
@@ -8,14 +10,25 @@ import br.com.mvabiguzzi.notasfiscais.modelo.Produto;
 @ManagedBean
 public class ProdutoBean {
 	private Produto produto = new Produto();
+	private List<Produto> produtos;
 
 	public Produto getProduto() {
-		return produto;
+		return this.produto;
+	}
+	
+	public List<Produto> getProdutos() {
+		if(this.produtos == null) {
+			System.out.println("Carregando produtos...");
+			this.produtos = new ProdutoDao().listaTodos();
+		}
+		
+		return this.produtos;
 	}
 	
 	public void grava() {
 		ProdutoDao pDao = new ProdutoDao();
-		pDao.adiciona(produto);
+		pDao.adiciona(this.produto);
 		this.produto = new Produto();
+		this.produtos = pDao.listaTodos();
 	}
 }
