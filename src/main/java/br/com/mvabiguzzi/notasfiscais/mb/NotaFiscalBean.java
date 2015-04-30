5c394020068907d45f1d307612d04dc79145cf12
Marcos Vinícius Almeida Biguzzi <contato@mvabiguzzi.com.br>
2015-04-30 15:43:36 -0300

    Criação do formulário para cadastro de notas fiscais

---
+++ src/main/java/br/com/mvabiguzzi/notasfiscais/mb/NotaFiscalBean.java
@@ -0,0 +1,69 @@
+package br.com.mvabiguzzi.notasfiscais.mb;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.mvabiguzzi.notasfiscais.dao.NotaFiscalDao;
+import br.com.mvabiguzzi.notasfiscais.dao.ProdutoDao;
+import br.com.mvabiguzzi.notasfiscais.modelo.Item;
+import br.com.mvabiguzzi.notasfiscais.modelo.NotaFiscal;
+import br.com.mvabiguzzi.notasfiscais.modelo.Produto;
+
+@Named @ViewScoped
+public class NotaFiscalBean implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Inject private NotaFiscalDao notaFiscalDao;
+	@Inject private ProdutoDao produtoDao;
+	
+	private NotaFiscal notaFiscal = new NotaFiscal();
+	private Item item = new Item();
+	private Long idProduto;
+	
+	public void grava() {
+		notaFiscalDao.adiciona(notaFiscal);
+		
+		notaFiscal = new NotaFiscal();
+	}
+	
+	public void guardaItem() {
+		Produto produto = produtoDao.buscaPorId(idProduto);
+		
+		item.setProduto(produto);
+		item.setValorUnitario(produto.getPreco());
+		
+		notaFiscal.getItens().add(item);
+		item.setNotaFiscal(notaFiscal);
+		
+		item = new Item();
+	}
+	
+	public NotaFiscal getNotaFiscal() {
+		return notaFiscal;
+	}
+
+	public void setNotaFiscal(NotaFiscal notaFiscal) {
+		this.notaFiscal = notaFiscal;
+	}
+	
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+
+	public Long getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
+	}
+	
+}
